7d96813550556891c84acc1268c436db8e6a1b7f
Jomy Thomas <jomythomas001@gmail.com>
2026-03-05 14:16:55 +0530

    initial app setup

---
+++ src/test/java/jt/lrn/data/redis/RedisConnectivityTest.java
@@ -0,0 +1,22 @@
+package jt.lrn.data.redis;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
+class RedisConnectivityTest {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+
+    @Test
+    void testSendMail() {
+        redisTemplate.opsForValue().set("email","gmail@email.com");
+        Object salary = redisTemplate.opsForValue().get("salary");
+        int a = 1;
+    }
+}
---
+++ src/test/java/jt/lrn/data/redis/RedisTestApplicationTests.java
@@ -0,0 +1,13 @@
+package jt.lrn.data.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
